fb81a7308600e35023a40dc6ace1a5ff67b032aa
Tom Noble <ttbnoble@gmail.com>
2020-01-24 16:48:52 +0000

    more basic changes

--- pages/search-results/index.vue
+++ pages/search-results/index.vue
@@ -14,6 +14,7 @@
       title="Inline Frame Example"
       width="100%"
       height="100%"
+      sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
       :src="iframeData"
     ></iframe>
   </div>
